790472a29260237834afad5b542d0251b947c3f4
thelegendaryboy64 <32961233+thelegendaryboy64@users.noreply.github.com>
2022-08-28 20:20:25 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 clear
 echo "If you ran this installer without root access, please press CTRL+C to stop this installer and add SUDO at the beginning before continuing with the installation."
 echo ""
-echo "Welcome to LightNexus! This is made specifically for Core 2 Duo era of PCs with 2GB RAM or more (in other words, older, weaker PCs), where little resource usage is key, yet maintaining a modern look for the UI."
+echo "Welcome to LightNexus! This is made for older, weaker PCs, where little resource usage is key, yet maintaining a modern look for the UI."
 echo ""
 read -p "If you're ready to install LightNexus, press ENTER, if not, stop the installer using CTRL+C"
 clear
@@ -25,6 +25,9 @@ echo "Repositories are installed. Now installing: PAMAC App Store"
 sudo pacman -Syu --noconfirm pamac-aur
 echo "Installing basic applications. Others can be installed later using the PAMAC App Store."
 sudo pacman -S --noconfirm vlc chromium libreoffice-fresh yaru-gtk-theme yaru-icon-theme yaru-metacity-theme yaru-sound-theme plank brisk-menu mate-applet-dock mate-netbook mate-menu mate-tweak compiz cutefish-wallpapers gvfs-smb
+echo "Changing wallpaper"
+gsettings set org.mate.background picture-filename '/usr/share/backgrounds/cutefishos/wallpaper-3.jpg'
+gsettings set org.mate.background picture-options 'zoom'
 echo "Installation done!"
 echo "Please refer to the link attached below on how to continue the installation of YAY to download extra applications if needed."
 read -p "https://github.com/thelegendaryboy64/LightNexus/blob/main/YAYInstallation.md"
