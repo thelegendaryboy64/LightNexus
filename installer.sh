d911230e448a1f18d7f92a2a9612a42c42ba0f7c
thelegendaryboy64 <32961233+thelegendaryboy64@users.noreply.github.com>
2022-08-28 21:52:52 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -25,9 +25,9 @@ echo "Repositories are installed. Now installing: PAMAC App Store"
 sudo pacman -Syu --noconfirm pamac-aur
 echo "Installing basic applications. Others can be installed later using the PAMAC App Store."
 sudo pacman -S --noconfirm linux-lts-headers vlc chromium libreoffice-fresh yaru-gtk-theme yaru-icon-theme yaru-metacity-theme yaru-sound-theme plank brisk-menu mate-applet-dock mate-netbook mate-menu mate-tweak compiz cutefish-wallpapers gvfs-smb
-echo "Changing wallpaper"
-gsettings set org.mate.background picture-filename '/usr/share/backgrounds/cutefishos/wallpaper-3.jpg'
-gsettings set org.mate.background picture-options 'zoom'
+# echo "Changing wallpaper"
+# gsettings set org.mate.background picture-filename '/usr/share/backgrounds/cutefishos/wallpaper-3.jpg'
+# gsettings set org.mate.background picture-options 'zoom'
 echo "Installation done!"
 echo "Please refer to the link attached below on how to continue the installation of YAY to download extra applications if needed."
 read -p "https://github.com/thelegendaryboy64/LightNexus/blob/main/YAYInstallation.md"
